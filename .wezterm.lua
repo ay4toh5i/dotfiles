45f95da11689765aad0128184d7eb145eca3a777
Ayato <ay4toh5i@gmail.com>
2022-09-25 17:48:59 +0900

    Add font setting in wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,9 @@
+local wezterm = require 'wezterm'
+
 return {
   color_scheme = 'iceberg-dark',
   window_background_opacity = 0.7,
   text_background_opacity = 0.7,
-  font_size = 13.0,
+  font_size = 16.0,
+  font = wezterm.font 'Ricty for Powerline',
 }
