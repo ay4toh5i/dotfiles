f8a9c26487b648e0331a73a84664753e4ab01401
Ayato <ay4toh5i@gmail.com>
2023-11-25 18:43:49 +0900

    Fix ibl cofig for v3

--- .config/nvim/lua/plugins/configs/indent-blankline.lua
+++ .config/nvim/lua/plugins/configs/indent-blankline.lua
@@ -1,9 +1,4 @@
 return { 
   'lukas-reineke/indent-blankline.nvim',
-  config = function () 
-    require('indent_blankline').setup {
-      show_current_context = true,
-      show_current_context_start = true,
-    }
-  end,
+  main = "ibl",
 }
