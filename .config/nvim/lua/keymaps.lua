a841cbc0ae72e98960e95a50f5f85067683e6e0a
Ayato <ay4toh5i@gmail.com>
2024-02-24 20:59:09 +0900

    Fix nvim key map

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -7,5 +7,5 @@ keymap('n', 'j', 'gj', { noremap = true })
 keymap('n', 'k', 'gk', { noremap = true })
 keymap('n', '<Esc><Esc>', ':nohlsearch<CR><Esc>', {})
 
-keymap('n', '<C-j>', ':bprev<CR>', {})
-keymap('n', '<C-k>', ':bnext<CR>', {})
+keymap('n', '<C-h>', ':bprev<CR>', {})
+keymap('n', '<C-l>', ':bnext<CR>', {})
