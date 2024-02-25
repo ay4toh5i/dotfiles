4efafce620fe29ec98dbe50f79395130fc0a8ac9
Ayato <ay4toh5i@gmail.com>
2024-02-25 18:42:23 +0900

    Fix lsp keymap

--- .config/nvim/lua/plugins/configs/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/configs/nvim-lspconfig.lua
@@ -33,7 +33,7 @@ return {
       vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
       vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
       vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-      vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+      vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
     end
 
     local lsp_flags = {
