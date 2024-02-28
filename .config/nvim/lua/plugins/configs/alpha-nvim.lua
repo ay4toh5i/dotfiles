eeab846ba1c027413b35b87c595e6e86190d559d
Ayato <ay4toh5i@gmail.com>
2024-02-28 21:43:26 +0900

    Add session manger key config

--- .config/nvim/lua/plugins/configs/alpha-nvim.lua
+++ .config/nvim/lua/plugins/configs/alpha-nvim.lua
@@ -1,9 +1,21 @@
 return {
   'goolord/alpha-nvim',
-  config       = function()
-    require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+  config = function()
+    local config = require('alpha.themes.dashboard').config
+    require('alpha').setup(config)
   end,
   dependencies = {
-    { 'Shatur/neovim-session-manager' }
+    {
+      'Shatur/neovim-session-manager',
+      config = function()
+        local config = require('session_manager.config')
+
+        require('session_manager').setup({
+          autoload_mode = config.AutoloadMode.Disabled,
+        })
+
+        vim.keymap.set('n', '<space>sl', '<cmd>SessionManager load_last_session<CR>', { silent = true })
+      end,
+    },
   },
 };
