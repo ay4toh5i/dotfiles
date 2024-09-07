5c1bd141a56dc2dc6b965b31e1189dc7d2ccc14b
Ayato <ay4toh5i@gmail.com>
2024-09-07 14:29:07 +0900

    Add telescope-egrepify, refine configuration

--- .config/nvim/lua/plugins/configs/telescope.lua
+++ .config/nvim/lua/plugins/configs/telescope.lua
@@ -3,14 +3,16 @@ return {
   branch = '0.1.x',
   config = function()
     local actions = require('telescope.actions')
+    local egrep_actions = require("telescope._extensions.egrepify.actions")
 
     require('telescope').setup({
       defaults = {
-      mappings = {
-        i = {
-          ['<C-s>'] = actions.send_selected_to_qflist + actions.open_qflist,
+        mappings = {
+          i = {
+            ['<C-s>'] = actions.send_selected_to_qflist + actions.open_qflist,
+            ['<C-h>'] = "which_key",
+          },
         },
-      },
         file_ignore_patterns = {
           "^.git/",
           "^.cache/",
@@ -27,11 +29,52 @@ return {
           "-uu",
         },
       },
+      extensions = {
+        egrepify = {
+          -- intersect tokens in prompt ala "str1.*str2" that ONLY matches
+          -- if str1 and str2 are consecutively in line with anything in between (wildcard)
+          AND = true,               -- default
+          permutations = false,     -- opt-in to imply AND & match all permutations of prompt tokens
+          lnum = true,              -- default, not required
+          lnum_hl = "EgrepifyLnum", -- default, not required, links to `Constant`
+          col = false,              -- default, not required
+          col_hl = "EgrepifyCol",   -- default, not required, links to `Constant`
+          title = true,             -- default, not required, show filename as title rather than inline
+          filename_hl = "EgrepifyFile", -- default, not required, links to `Title`
+          results_ts_hl = false,    -- set to true if you want results ts highlighting, may increase latency!
+          -- suffix = long line, see screenshot
+          -- EXAMPLE ON HOW TO ADD PREFIX!
+          prefixes = {
+            -- ADDED ! to invert matches
+            -- example prompt: ! sorter
+            -- matches all lines that do not comprise sorter
+            -- rg --invert-match -- sorter
+            ["!"] = {
+              flag = "invert-match",
+            },
+            -- HOW TO OPT OUT OF PREFIX
+            -- ^ is not a default prefix and safe example
+            ["^"] = false
+          },
+          -- default mappings
+          mappings = {
+            i = {
+              -- toggle prefixes, prefixes is default
+              ["<C-z>"] = egrep_actions.toggle_prefixes,
+              -- toggle AND, AND is default, AND matches tokens and any chars in between
+              ["<C-a>"] = egrep_actions.toggle_and,
+              -- toggle permutations, permutations of tokens is opt-in
+              ["<C-r>"] = egrep_actions.toggle_permutations,
+            },
+          },
+        },
+      },
     })
   end,
   keys = {
     { "<leader>ff", ":Telescope find_files hidden=true<CR>" },
-    { "<leader>fg", ":Telescope live_grep<CR>" },
+    { "<leader>fg", ":Telescope egrepify<CR>" },
+    { "<leader>fb", ":Telescope buffers<CR>" },
     { "<leader>of", ":Telescope oldfiles<CR>" },
   },
   dependencies = { 'nvim-lua/plenary.nvim' },
