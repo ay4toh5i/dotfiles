60d404d3079e4f50701c23c91f5daf9388e7c457
Ayato <ay4toh5i@gmail.com>
2024-09-23 11:45:34 +0900

    Fix trouble and lualine config

--- .config/nvim/lua/plugins/configs/lualine.lua
+++ .config/nvim/lua/plugins/configs/lualine.lua
@@ -2,11 +2,28 @@ return {
   'nvim-lualine/lualine.nvim',
   config = function()
     require('lualine').setup({
-      options = { 
+      options = {
         theme = 'nordic',
         globalstatus = true,
       },
       extensions = { 'neo-tree' },
+    })
+
+    local opts = require('lualine').get_config()
+    local trouble = require("trouble")
+    local symbols = trouble.statusline({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+      -- The following line is needed to fix the background color
+      -- Set it to the lualine section you want to use
+      hl_group = "lualine_c_normal",
+    })
+    table.insert(opts.sections.lualine_c, {
+      symbols.get,
+      cond = symbols.has,
     })
   end,
 }
