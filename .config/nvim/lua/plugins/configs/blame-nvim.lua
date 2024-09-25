c129bf55437ff56e3f3f46123f8b444f21783e27
Ayato <ay4toh5i@gmail.com>
2024-09-25 09:30:21 +0900

    Add keymap

--- .config/nvim/lua/plugins/configs/blame-nvim.lua
+++ .config/nvim/lua/plugins/configs/blame-nvim.lua
@@ -1,3 +1,7 @@
 return {
   'FabijanZulj/blame.nvim',
+  config = function()
+    require('blame').setup()
+    vim.keymap.set('n', '<leader>gb', '<cmd>BlameToggle<cr>', { noremap = true, silent = true })
+  end
 }
