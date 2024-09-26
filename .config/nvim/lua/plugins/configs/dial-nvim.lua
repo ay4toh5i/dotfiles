b88bd8e16bea82bd3b9fdece820bb84db6c9eafb
Ayato <ay4toh5i@gmail.com>
2024-09-26 22:08:35 +0900

    Add dial.nvim config

--- .config/nvim/lua/plugins/configs/dial-nvim.lua
+++ .config/nvim/lua/plugins/configs/dial-nvim.lua
@@ -1,6 +1,19 @@
 return {
   'monaqa/dial.nvim',
   config = function()
+    local augend = require("dial.augend")
+    require("dial.config").augends:register_group {
+      -- default augends used when no group name is specified
+      default = {
+        augend.integer.alias.decimal,  -- nonnegative decimal number (0, 1, 2, 3, ...)
+        augend.integer.alias.hex,      -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+        augend.date.alias["%Y/%m/%d"], -- date (2022/02/19, etc.)
+        augend.constant.alias.bool,    -- boolean value (true <-> false)
+        augend.constant.alias.alpha,
+        augend.constant.alias.Alpha,
+      },
+    }
+
     local dial = require("dial.map")
     vim.keymap.set("n", "<C-a>", function() dial.manipulate("increment", "normal") end)
     vim.keymap.set("n", "<C-x>", function() dial.manipulate("decrement", "normal") end)
