82af87417f1acf426de296a3c723c94cce29a866
Ayato <ay4toh5i@gmail.com>
2024-10-05 13:59:29 +0900

    Add diffview keymap

--- .config/nvim/lua/plugins/configs/diffview.lua
+++ .config/nvim/lua/plugins/configs/diffview.lua
@@ -1,3 +1,9 @@
 return {
-    'sindrets/diffview.nvim'
+  'sindrets/diffview.nvim',
+  config = function()
+    vim.keymap.set('n', '<leader>gdo', '<cmd>DiffviewOpen<cr>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gdc', '<cmd>DiffviewClose<cr>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>gdh', '<cmd>DiffviewFileHistory %<cr>', { noremap = true, silent = true })
+    vim.keymap.set('x', 'gdh', ":DiffviewFileHistory<cr>", { noremap = true, silent = true })
+  end
 }
