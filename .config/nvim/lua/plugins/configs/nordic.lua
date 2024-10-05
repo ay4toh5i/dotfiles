22ea86f70462b67b93a2d40a1c8bb56c450db8ea
Ayato <ay4toh5i@gmail.com>
2024-10-06 01:07:11 +0900

    Fix nordic config

--- .config/nvim/lua/plugins/configs/nordic.lua
+++ .config/nvim/lua/plugins/configs/nordic.lua
@@ -2,15 +2,11 @@ return {
   'AlexvZyl/nordic.nvim',
   priority = 10000,
   config = function()
-    local palette = require('nordic.colors')
-
-    require('nordic').setup(
-      {
-        override = {
-          Visual = { bg = palette.gray2 },
-          PmenuSel = { bg = palette.gray2 },
-        },
-      }
-    )
+    require('nordic').setup({
+      on_highlight = function(highlights, palette)
+        highlights.Visual = { bg = palette.gray2 }
+        highlights.PmenuSel = { bg = palette.gray2 }
+      end,
+    })
   end
 }
