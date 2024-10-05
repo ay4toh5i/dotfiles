bc10edc31a60669d227c0f921b8e7f9a1ac57eb5
Ayato <ay4toh5i@gmail.com>
2024-10-06 01:29:11 +0900

    Add project manager plugin

--- .config/nvim/lua/plugins/configs/alpha-nvim.lua
+++ .config/nvim/lua/plugins/configs/alpha-nvim.lua
@@ -5,17 +5,6 @@ return {
     require('alpha').setup(config)
   end,
   dependencies = {
-    {
-      'Shatur/neovim-session-manager',
-      config = function()
-        local config = require('session_manager.config')
-
-        require('session_manager').setup({
-          autoload_mode = config.AutoloadMode.Disabled,
-        })
-
-        vim.keymap.set('n', '<space>sl', '<cmd>SessionManager load_last_session<CR>', { silent = true })
-      end,
-    },
+    { 'Shatur/neovim-session-manager' },
   },
 };
---
+++ .config/nvim/lua/plugins/configs/neovim-project.lua
@@ -0,0 +1,35 @@
+return {
+  "coffebar/neovim-project",
+  config = function()
+    local config = require('session_manager.config')
+
+    require('neovim-project').setup({
+      projects = { -- define project roots
+        '~/dotfiles',
+      },
+      picker = {
+        type = "telescope", -- or "fzf-lua"
+      },
+      last_session_on_startup = false,
+      session_manager_opts = {
+        autoload = config.AutoloadMode.Disabled,
+      }
+    })
+
+    -- enable saving the state of plugins in the session
+    vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+
+    local subcommands = require('session_manager.subcommands')
+    vim.api.nvim_create_user_command('SessionManager', subcommands.run,
+      { nargs = '*', bang = true, complete = subcommands.complete, desc = 'Run session manager command' })
+
+    vim.keymap.set('n', '<space>sl', ':SessionManager load_last_session<CR>', { silent = true })
+  end,
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+    { "Shatur/neovim-session-manager" },
+  },
+  lazy = false,
+  priority = 100,
+}
