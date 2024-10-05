f6814755da3a54dd03e1d463bd5a4f87b58e92af
Ayato <ay4toh5i@gmail.com>
2024-10-06 01:58:51 +0900

    Fix project manager config

--- .config/nvim/lua/plugins/configs/neovim-project.lua
+++ .config/nvim/lua/plugins/configs/neovim-project.lua
@@ -3,17 +3,27 @@ return {
   config = function()
     local config = require('session_manager.config')
 
+    local result = vim.system({ 'ghq', 'root' }):wait()
+    if result.code ~= 0 then
+      vim.notify('Failed to get ghq root path', 'error', {
+        title = 'Neovim Project',
+      })
+    end
+
+    local ghq_root = vim.trim(result.stdout)
+
     require('neovim-project').setup({
       projects = { -- define project roots
         '~/dotfiles',
+        ghq_root .. '/github.com/*/*',
       },
       picker = {
         type = "telescope", -- or "fzf-lua"
       },
       last_session_on_startup = false,
       session_manager_opts = {
-        autoload = config.AutoloadMode.Disabled,
-      }
+        autoload_mode = config.AutoloadMode.Disabled,
+      },
     })
 
     -- enable saving the state of plugins in the session
@@ -23,7 +33,8 @@ return {
     vim.api.nvim_create_user_command('SessionManager', subcommands.run,
       { nargs = '*', bang = true, complete = subcommands.complete, desc = 'Run session manager command' })
 
-    vim.keymap.set('n', '<space>sl', ':SessionManager load_last_session<CR>', { silent = true })
+    vim.keymap.set('n', '<space>sl', ':SessionManager load_current_dir_session<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>pj', ':NeovimProjectDiscover<CR>', { silent = true })
   end,
   dependencies = {
     { "nvim-lua/plenary.nvim" },
