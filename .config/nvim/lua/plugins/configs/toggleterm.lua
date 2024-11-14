b3b5ca851ac98590239294fa8035301d856a2c55
Ayato <ay4toh5i@gmail.com>
2024-11-14 22:37:56 +0900

    Fix keymap

--- .config/nvim/lua/plugins/configs/toggleterm.lua
+++ .config/nvim/lua/plugins/configs/toggleterm.lua
@@ -5,6 +5,6 @@ return {
     require('toggleterm').setup({})
     vim.keymap.set('n', '<space>ot', ':ToggleTerm direction=float<CR>')
     vim.keymap.set('n', '<C-T>', ':ToggleTerm direction=float<CR>', { silent = true })
-    vim.keymap.set('t', '<C-T>', '<C-\\><C-n>:FloatermToggle<CR>:ToggleTerm direction=float<CR>', { silent = true })
+    vim.keymap.set('t', '<C-T>', '<C-\\><C-n>:ToggleTerm direction=float<CR>', { silent = true })
   end,
 }
