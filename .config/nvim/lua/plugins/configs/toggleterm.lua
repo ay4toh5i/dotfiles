417bc0d77784b124cbf920a9ceb0d3b42b34fe88
Ayato <ay4toh5i@gmail.com>
2024-11-16 21:53:22 +0900

    Make the toggleterm always start in insert mode

--- .config/nvim/lua/plugins/configs/toggleterm.lua
+++ .config/nvim/lua/plugins/configs/toggleterm.lua
@@ -6,5 +6,15 @@ return {
     vim.keymap.set('n', '<space>ot', ':ToggleTerm direction=float<CR>')
     vim.keymap.set('n', '<C-T>', ':ToggleTerm direction=float<CR>', { silent = true })
     vim.keymap.set('t', '<C-T>', '<C-\\><C-n>:ToggleTerm direction=float<CR>', { silent = true })
+
+    -- Start insert mode when entering terminal
+    local augroup_term_insert = vim.api.nvim_create_augroup("Term-Insert", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "TermOpen", "TermEnter" }, {
+      group = augroup_term_insert,
+      pattern = 'term://*',
+      callback = function()
+        vim.cmd('startinsert')
+      end
+    })
   end,
 }
