f3d807c271f6b31c6e3f23a5fa78b3efeb6d862f
Ayato <ay4toh5i@gmail.com>
2024-11-24 23:02:45 +0900

    Change toggleterm keybinding

--- .config/nvim/lua/plugins/configs/toggleterm.lua
+++ .config/nvim/lua/plugins/configs/toggleterm.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     require('toggleterm').setup({})
     vim.keymap.set('n', '<space>ot', ':ToggleTerm direction=float<CR>')
-    vim.keymap.set('n', '<C-T>', ':ToggleTerm direction=float<CR>', { silent = true })
-    vim.keymap.set('t', '<C-T>', '<C-\\><C-n>:ToggleTerm direction=float<CR>', { silent = true })
+    vim.keymap.set('n', '<M-t>', ':ToggleTerm direction=float<CR>', { noremap = true, silent = true })
+    vim.keymap.set('t', '<M-t>', '<C-\\><C-n>:ToggleTerm direction=float<CR>', { noremap = true, silent = true })
 
     -- Start insert mode when entering terminal
     local augroup_term_insert = vim.api.nvim_create_augroup("Term-Insert", { clear = true })
