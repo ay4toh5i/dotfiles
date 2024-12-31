3dac8e1c6f81e34557d86346ff44ae8ba57f940f
Ayato <ay4toh5i@gmail.com>
2025-01-01 00:24:04 +0900

    Add keymap

--- .config/nvim/lua/plugins/configs/overseer.lua
+++ .config/nvim/lua/plugins/configs/overseer.lua
@@ -3,5 +3,6 @@ return {
   config = function()
     require('overseer').setup()
     vim.keymap.set('n', '<leader>rr', ':OverseerRun<CR>')
+    vim.keymap.set('n', '<leader>rt', ':OverseerToggle<CR>')
   end,
 }
