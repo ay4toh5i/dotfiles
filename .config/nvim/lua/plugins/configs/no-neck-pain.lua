3c175da4f9aaf3caba8837cd0f970f8d6fddafda
Ayato <ay4toh5i@gmail.com>
2025-01-23 17:11:59 +0900

    Fix no-neck-pain config

--- .config/nvim/lua/plugins/configs/no-neck-pain.lua
+++ .config/nvim/lua/plugins/configs/no-neck-pain.lua
@@ -2,7 +2,12 @@ return {
   'shortcuts/no-neck-pain.nvim',
   config = function()
     require('no-neck-pain').setup({
-      width = 120,
+      width = 125,
+      buffers = {
+        wo = {
+          fillchars = "eob: ",
+        },
+      }
     })
 
     vim.keymap.set('n', '<leader>cc', ':NoNeckPain<CR>')
