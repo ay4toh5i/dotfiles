5e11799d2b7eba28abe3476c5a8f101c91255c66
Ayato <ay4toh5i@gmail.com>
2025-02-01 21:01:58 +0900

    Fix trouble config

--- .config/nvim/lua/plugins/configs/trouble.lua
+++ .config/nvim/lua/plugins/configs/trouble.lua
@@ -6,8 +6,10 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     use_diagnostic_signs = true,
+    auto_close = true,
+    focus = true,
   },
- keys = {
+  keys = {
     {
       "<leader>xx",
       "<cmd>Trouble diagnostics toggle<cr>",
