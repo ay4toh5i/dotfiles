62e5a9696841690f0a541ab6e30131f983ebd57d
Ayato <ay4toh5i@gmail.com>
2025-02-01 21:57:54 +0900

    Use copilot.lua instead of copilot.vim

--- .config/nvim/lua/plugins/configs/copilot.lua
+++ .config/nvim/lua/plugins/configs/copilot.lua
@@ -1,3 +1,17 @@
 return {
-  'github/copilot.vim',
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup({
+      panel = {
+        layout = {
+          position = 'right',
+        },
+      },
+      suggestion = {
+        auto_trigger = true,
+      },
+    })
+  end
 }
