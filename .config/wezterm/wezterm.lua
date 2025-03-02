5bc7db7fac281536846fd2c10db7159c95a3fc37
Ayato <ay4toh5i@gmail.com>
2025-03-02 13:08:39 +0900

    Fix font setting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -60,9 +60,9 @@ return {
   text_background_opacity = 0.95,
   font_size = 14.0,
   font = wezterm.font_with_fallback {
-    "PlemolJP Console NF",
-    'Ricty for Powerline',
+    '0xProto Nerd Font',
     'JetBrains Mono',
+    "PlemolJP Console NF",
   },
   keys = keys,
   hide_tab_bar_if_only_one_tab = true,
