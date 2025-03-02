7661cd254ec658a847e044d50fd2419968b27243
Ayato <ay4toh5i@gmail.com>
2025-03-02 11:41:49 +0900

    Enable telescope results highlight

--- .config/nvim/lua/plugins/configs/telescope.lua
+++ .config/nvim/lua/plugins/configs/telescope.lua
@@ -49,7 +49,7 @@ return {
           col_hl = "EgrepifyCol",       -- default, not required, links to `Constant`
           title = true,                 -- default, not required, show filename as title rather than inline
           filename_hl = "EgrepifyFile", -- default, not required, links to `Title`
-          results_ts_hl = false,        -- set to true if you want results ts highlighting, may increase latency!
+          results_ts_hl = true,        -- set to true if you want results ts highlighting, may increase latency!
           -- suffix = long line, see screenshot
           -- EXAMPLE ON HOW TO ADD PREFIX!
           prefixes = {
