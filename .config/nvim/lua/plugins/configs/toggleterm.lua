1c233749759d327c169ca088291369a165e8afa0
Ayato <ay4toh5i@gmail.com>
2025-03-03 23:21:31 +0900

    Fix toggleterm config to open terminal as insert mode always

--- .config/nvim/lua/plugins/configs/toggleterm.lua
+++ .config/nvim/lua/plugins/configs/toggleterm.lua
@@ -2,19 +2,34 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   config = function()
-    require('toggleterm').setup({})
-    vim.keymap.set('n', '<space>ot', ':ToggleTerm direction=float<CR>')
-    vim.keymap.set('n', '<M-t>', ':ToggleTerm direction=float<CR>', { noremap = true, silent = true })
-    vim.keymap.set('t', '<M-t>', '<C-\\><C-n>:ToggleTerm direction=float<CR>', { noremap = true, silent = true })
-
-    -- Start insert mode when entering terminal
-    local augroup_term_insert = vim.api.nvim_create_augroup("Term-Insert", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "WinEnter", "TermOpen", "TermEnter" }, {
-      group = augroup_term_insert,
-      pattern = 'term://*',
-      callback = function()
-        vim.cmd('startinsert')
-      end
+    require('toggleterm').setup({
+      direction = 'float',
+      start_in_insert = true,
+      persist_mode = false,
+      float_opts = {
+        border = 'curved',
+      }
     })
   end,
+  keys = {
+    {
+      '<space>ot',
+      ':ToggleTerm<CR>',
+      desc = 'Toggle terminal',
+      silent = true,
+    },
+    {
+      '<M-t>',
+      ':ToggleTerm<CR>',
+      desc = 'Toggle terminal',
+      silent = true,
+    },
+    {
+      '<M-t>',
+      '<C-\\><C-n>:ToggleTerm<CR>',
+      mode = 't',
+      desc = 'Toggle terminal',
+      silent = true,
+    },
+  },
 }
