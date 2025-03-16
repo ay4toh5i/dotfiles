d924ca5917017a60f86f4f05fe0817fa527635a5
Ayato <ay4toh5i@gmail.com>
2025-03-16 12:09:23 +0900

    Improve neovim-project and session-manager config

--- .config/nvim/lua/plugins/configs/neovim-project.lua
+++ .config/nvim/lua/plugins/configs/neovim-project.lua
@@ -1,11 +1,13 @@
 return {
   "coffebar/neovim-project",
+  init = function()
+    -- enable saving the state of plugins in the session
+    vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+  end,
   config = function()
-    local config = require('session_manager.config')
-
     local result = vim.system({ 'ghq', 'root' }):wait()
     if result.code ~= 0 then
-      vim.notify('Failed to get ghq root path', 'error', {
+      vim.notify('Failed to get ghq root path', vim.log.levels.ERROR, {
         title = 'Neovim Project',
       })
     end
@@ -22,25 +24,65 @@ return {
       },
       last_session_on_startup = false,
       dashboard_mode = true,
-      session_manager_opts = {
-        autosave_last_session = true,
-      },
     })
 
-    -- enable saving the state of plugins in the session
-    vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-
-    local subcommands = require('session_manager.subcommands')
-    vim.api.nvim_create_user_command('SessionManager', subcommands.run,
-      { nargs = '*', bang = true, complete = subcommands.complete, desc = 'Run session manager command' })
-
-    vim.keymap.set('n', '<space>sl', ':SessionManager load_current_dir_session<CR>', { silent = true })
-    vim.keymap.set('n', '<leader>pj', ':NeovimProjectDiscover<CR>', { silent = true })
+    vim.keymap.set('n', '<leader>pj', ':NeovimProjectDiscover<CR>',
+      {
+        silent = true,
+        desc = 'Discover projects',
+      }
+    )
   end,
   dependencies = {
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope.nvim" },
-    { "Shatur/neovim-session-manager" },
+    {
+      "Shatur/neovim-session-manager",
+      config = function()
+        require('session_manager').setup({
+          autosave_last_session = true,
+        })
+
+        local subcommands = require('session_manager.subcommands')
+        vim.api.nvim_create_user_command(
+          'SessionManager',
+          subcommands.run,
+          {
+            nargs = '*',
+            bang = true,
+            complete = subcommands.complete,
+            desc = 'Run session manager command',
+          }
+        )
+
+        local sm = require('session_manager')
+
+        -- Auto save session
+        vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+          callback = function()
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+              -- Don't save while there's any 'nofile' buffer open.
+              if vim.api.nvim_get_option_value("buftype", { buf = buf }) == 'nofile' then
+                return
+              end
+            end
+            sm.save_current_session()
+          end
+        })
+
+        vim.keymap.set('n', '<space>sl',
+          function()
+            if not sm.load_git_session() then
+              sm.load_current_dir_session()
+            end
+          end,
+          {
+            silent = true,
+            desc = 'Load session from git or current directory',
+          }
+        )
+      end,
+    },
   },
   lazy = false,
   priority = 100,
