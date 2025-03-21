4b1c8108f4b0f2822a959829b7b9a738295b710a
Ayato <ay4toh5i@gmail.com>
2025-03-22 01:32:17 +0900

    Add treesitter textsubjects

---
+++ .config/nvim/lua/plugins/configs/nvim-treesitter-textsubjects.lua
@@ -0,0 +1,13 @@
+return {
+  'rrethy/nvim-treesitter-textsubjects',
+  config = function()
+    require('nvim-treesitter-textsubjects').configure({
+      prev_selection = ',',
+      keymaps = {
+        ['.'] = 'textsubjects-smart',
+        [';'] = 'textsubjects-container-outer',
+        ['i;'] = 'textsubjects-container-inner',
+      },
+    })
+  end
+}
