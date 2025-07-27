6f298b43e4e7ca62bd0c9cc9f65c88ee5c99726e
Ayato <ay4toh5i@gmail.com>
2025-07-27 19:08:02 +0900

    Remove unused plugins

--- .config/nvim/lua/plugins/configs/blame-nvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'FabijanZulj/blame.nvim',
-  config = function()
-    require('blame').setup()
-    vim.keymap.set('n', '<leader>gb', '<cmd>BlameToggle<cr>', { noremap = true, silent = true })
-  end
-}
