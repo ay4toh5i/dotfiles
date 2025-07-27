79dcfdfe9a7c4081a84cc2f96e1c3f0cb14b5b02
Ayato <ay4toh5i@gmail.com>
2025-07-27 19:08:31 +0900

    Add the gitsigns keybindings

--- .config/nvim/lua/plugins/configs/gitsigns.lua
+++ .config/nvim/lua/plugins/configs/gitsigns.lua
@@ -1,6 +1,12 @@
 return {
-    'lewis6991/gitsigns.nvim',
-    config = function ()
-      require('gitsigns').setup()
-    end
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+  end,
+  keys = {
+    { '<leader>gb', '<cmd>Gitsigns blame<cr>',        desc = 'Blame' },
+    { '<leader>gh', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Preview Hunk' },
+    { '<leader>gr', '<cmd>Gitsigns reset_hunk<cr>',   desc = 'Reset Hunk' },
+    { '<leader>gR', '<cmd>Gitsigns reset_buffer<cr>', desc = 'Reset Buffer' },
+  },
 }
