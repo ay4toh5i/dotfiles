96d61445960bc9769e8346184645424f81964a3e
Ayato <ay4toh5i@gmail.com>
2025-07-27 20:17:48 +0900

    Add trace-pr.nvim

---
+++ .config/nvim/lua/plugins/configs/trace-pr.lua
@@ -0,0 +1,11 @@
+return {
+  "h3pei/trace-pr.nvim",
+  config = true,
+  keys = {
+    {
+      "<leader>gp",
+      "<cmd>TracePR<cr>",
+      desc = "Open Pull Request related to current line",
+    },
+  },
+}
