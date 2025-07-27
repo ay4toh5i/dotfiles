6ebf2c49ee5a547b0f0d75ac283aaefcde564f58
Ayato <ay4toh5i@gmail.com>
2025-07-27 19:40:09 +0900

    Fix gitlinker.nvim keymap

--- .config/nvim/lua/plugins/configs/gitlinker.lua
+++ .config/nvim/lua/plugins/configs/gitlinker.lua
@@ -2,12 +2,10 @@ return {
   'ruifm/gitlinker.nvim',
   config = function()
     require('gitlinker').setup()
-    vim.api.nvim_set_keymap(
-      'n', '<leader>gl',
-      '<cmd>lua require("gitlinker").get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-      { silent = true }
-    )
   end,
+  keys = {
+    { '<leader>go', '<cmd>lua require("gitlinker").get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', desc = 'Get Git Link' },
+  },
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
