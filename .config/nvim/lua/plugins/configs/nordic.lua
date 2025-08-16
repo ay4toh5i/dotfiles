438eb6fd0e669427d7a72953d0a4319569c31181
Ayato <ay4toh5i@gmail.com>
2025-08-16 13:39:54 +0900

    Fix nordic diff colors, when using transparent background

--- .config/nvim/lua/plugins/configs/nordic.lua
+++ .config/nvim/lua/plugins/configs/nordic.lua
@@ -2,9 +2,19 @@ return {
   'AlexvZyl/nordic.nvim',
   priority = 10000,
   config = function()
+    local U = require('nordic.utils')
+
     require('nordic').setup({
       on_palette = function() end,
-      after_palette = function () end,
+      after_palette = function(palette)
+        local diff_blend = 0.6
+        palette.diff = {
+          change0 = U.blend(palette.blue1, palette.gray0, diff_blend / 2),
+          change1 = U.blend(palette.blue2, palette.gray0, diff_blend),
+          add = U.blend(palette.green.base, palette.gray0, diff_blend),
+          delete = U.blend(palette.red.base, palette.gray0, diff_blend),
+        }
+      end,
       on_highlight = function(highlights, palette)
         highlights.Visual = { bg = palette.gray2 }
         highlights.PmenuSel = { bg = palette.gray2 }
