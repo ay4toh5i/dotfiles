54406d78164d7c26cb2c101f66400b8077bd65db
Ayato <ay4toh5i@gmail.com>
2025-10-19 16:04:12 +0900

    Add deno inlay hints configuration

--- .config/nvim/after/lsp/denols.lua
+++ .config/nvim/after/lsp/denols.lua
@@ -47,4 +47,12 @@ return {
     },
   },
   workspace_required = false,
+  inlayHints = {
+    parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = true },
+    parameterTypes = { enabled = true },
+    variableTypes = { enabled = true, suppressWhenTypeMatchesName = true },
+    propertyDeclarationTypes = { enabled = true },
+    functionLikeReturnTypes = { enable = true },
+    enumMemberValues = { enabled = true },
+  },
 }
