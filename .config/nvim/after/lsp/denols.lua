ca1303a371e3fcee56c864ec954c72ed7563377c
Ayato <ay4toh5i@gmail.com>
2025-10-19 16:04:05 +0900

    Fix indent

--- .config/nvim/after/lsp/denols.lua
+++ .config/nvim/after/lsp/denols.lua
@@ -1,32 +1,32 @@
 -- detach denols if vtsls or tsserver is running. If no buffer is attached to denols, stop the client
 require("core.plugin").on_attach(function(client, bufnr)
-	local node_servers = { "vtsls", "tsserver", "ts_ls" }
+  local node_servers = { "vtsls", "tsserver", "ts_ls" }
 
-	if not vim.iter({ node_servers, "denols" }):flatten(math.huge):any(function(s)
-		return client.name == s
-	end) then
-		return
-	end
+  if not vim.iter({ node_servers, "denols" }):flatten(math.huge):any(function(s)
+        return client.name == s
+      end) then
+    return
+  end
 
-	-- if client.root_dir ~= nil then
-	-- 	return
-	-- end
+  -- if client.root_dir ~= nil then
+  -- 	return
+  -- end
 
-	vim.schedule(function()
-		---@type vim.lsp.Client[]
-		local nodeLSPs = vim.iter({ "vtsls", "tsserver", "ts_ls" })
-			:map(function(cn)
-				return vim.lsp.get_clients({ name = cn, bufnr = bufnr })
-			end)
-			:flatten()
-			:totable()
-		local denoLSPs = vim.lsp.get_clients({ name = "denols", bufnr = bufnr })
-		if #nodeLSPs > 0 and #denoLSPs > 0 then
-			vim.iter(denoLSPs):each(function(denoLSP)
-				vim.lsp.buf_detach_client(bufnr, denoLSP.id)
-			end)
-		end
-	end)
+  vim.schedule(function()
+    ---@type vim.lsp.Client[]
+    local nodeLSPs = vim.iter({ "vtsls", "tsserver", "ts_ls" })
+        :map(function(cn)
+          return vim.lsp.get_clients({ name = cn, bufnr = bufnr })
+        end)
+        :flatten()
+        :totable()
+    local denoLSPs = vim.lsp.get_clients({ name = "denols", bufnr = bufnr })
+    if #nodeLSPs > 0 and #denoLSPs > 0 then
+      vim.iter(denoLSPs):each(function(denoLSP)
+        vim.lsp.buf_detach_client(bufnr, denoLSP.id)
+      end)
+    end
+  end)
 end)
 
 ---@type vim.lsp.Config
