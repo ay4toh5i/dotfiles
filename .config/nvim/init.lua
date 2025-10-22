53de9f17930f87d904d2d5170185435aafbed0db
Ayato <ay4toh5i@gmail.com>
2025-10-22 18:36:32 +0900

    Fix highlight

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,4 +3,6 @@ require('keymaps')
 require('plugins')
 
 vim.cmd.colorscheme('nordic')
-vim.api.nvim_set_hl(0, "OilDirIcon", { fg = "#E0AF68" }) -- Set custom color for directory icon
+
+vim.api.nvim_set_hl(0, "OilDirIcon", { fg = "#E0AF68" })         -- Set custom color for directory icon
+vim.api.nvim_set_hl(0, "SagaBorder", { link = "FloatBorder" })
