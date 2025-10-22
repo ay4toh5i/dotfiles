c3a118149854a7583c7e201e9a89a9b2dc27a19f
Ayato <ay4toh5i@gmail.com>
2025-10-22 19:13:33 +0900

    Make illuminate ignore more filetypes

--- .config/nvim/lua/plugins/configs/vim-illuminate.lua
+++ .config/nvim/lua/plugins/configs/vim-illuminate.lua
@@ -1,3 +1,19 @@
 return {
-    'RRethy/vim-illuminate',
+  'RRethy/vim-illuminate',
+  config = function()
+    require('illuminate').configure({
+      filetypes_denylist = {
+        'toggleterm',
+        'TelescopePrompt',
+        'alpha',
+        'lazy',
+        'mason',
+        'notify',
+        'packer',
+        'spectre_panel',
+        'which-key',
+        'neo-tree',
+      },
+    })
+  end,
 }
