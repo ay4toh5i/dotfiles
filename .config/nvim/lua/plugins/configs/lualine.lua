53dccc65b5a5f733e91da0fef926a7db66b2b2b6
Ayato <ay4toh5i@gmail.com>
2025-10-23 01:06:35 +0900

    Refactor lualine config and add overseer integration

--- .config/nvim/lua/plugins/configs/lualine.lua
+++ .config/nvim/lua/plugins/configs/lualine.lua
@@ -7,44 +7,20 @@ local icons = {
   lua_ls = '',
 }
 
+local lsp_names = function()
+  local clients = vim
+      .iter(vim.lsp.get_clients { bufnr = 0 })
+      :map(function(client)
+        return icons[client.name] or client.name
+      end)
+      :totable()
+
+  return ' ' .. table.concat(clients, ', ')
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   config = function()
-    local lsp_names = function()
-      local clients = vim
-          .iter(vim.lsp.get_clients { bufnr = 0 })
-          :map(function(client)
-            if client.name == 'null-ls' then
-              return ('null-ls(%s)'):format(table.concat(
-                vim
-                .iter(require('null-ls.sources').get_available(vim.bo.filetype))
-                :map(function(source)
-                  return source.name
-                end)
-                :totable(),
-                ' '
-              ))
-            else
-              return icons[client.name] or client.name
-            end
-          end)
-          :totable()
-
-      return ' ' .. table.concat(clients, ', ')
-    end
-
-    require('lualine').setup({
-      options = {
-        theme = 'nordic',
-        globalstatus = true,
-      },
-      extensions = { 'neo-tree' },
-      sections = {
-        lualine_x = { lsp_names },
-      },
-    })
-
-    local opts = require('lualine').get_config()
     local trouble = require("trouble")
     local symbols = trouble.statusline({
       mode = "lsp_document_symbols",
@@ -56,9 +32,19 @@ return {
       -- Set it to the lualine section you want to use
       hl_group = "lualine_c_normal",
     })
-    table.insert(opts.sections.lualine_c, {
-      symbols.get,
-      cond = symbols.has,
+
+    local lsp_symbols = { symbols.get, cond = symbols.has }
+
+    require('lualine').setup({
+      options = {
+        theme = 'nordic',
+        globalstatus = true,
+      },
+      extensions = { 'neo-tree' },
+      sections = {
+        lualine_c = { 'filename', lsp_symbols },
+        lualine_x = { 'overseer', lsp_names, 'encoding', 'filetype' },
+      },
     })
   end,
 }
