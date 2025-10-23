399db9632a5a574b1f5d7d6158733f49a4153dca
Ayato <ay4toh5i@gmail.com>
2025-10-23 13:29:57 +0900

    Fix highlighting for illuminate

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -1,9 +1,18 @@
 vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = '*',
   callback = function()
+    local palette = require('nordic.colors')
+
     vim.api.nvim_set_hl(0, "OilDirIcon", { fg = "#E0AF68" }) -- Set custom color for directory icon
 
     vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'None', fg = 'None' })
+
+    -- nvimdev/lspsaga.nvim
     vim.api.nvim_set_hl(0, "SagaBorder", { link = "FloatBorder" })
+
+    -- RRethy/vim-illuminate
+    vim.api.nvim_set_hl(0, 'IlluminatedWordText', { bg = palette.gray1, bold = true })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { bg = palette.gray1, bold = true })
+    vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { bg = palette.gray1, bold = true })
   end,
 })
