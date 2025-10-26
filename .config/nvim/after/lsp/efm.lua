0c03396cb60a932bd969d232736adb30741f6518
Ayato <ay4toh5i@gmail.com>
2025-10-26 23:22:31 +0900

    Make textlint run via deno

--- .config/nvim/after/lsp/efm.lua
+++ .config/nvim/after/lsp/efm.lua
@@ -8,10 +8,26 @@ return {
   },
   settings = {
     rootMarkers = { ".git/" },
+    lintAfterOpen = true,
     languages = {
       markdown = {
         require('efmls-configs.linters.markdownlint'),
-        require('efmls-configs.linters.textlint'),
+        -- require('efmls-configs.linters.textlint'),
+        {
+          prefix = "textlint",
+          lintCommand =
+          "deno run -A npm:textlint@latest --no-color --format unix --stdin --stdin-filename ${INPUT}",
+          lintStdin = true,
+          lintAfterOpen = true,
+          lintFormats = {
+            [[%E%f:%l:%c: %m \[%t%*[^/]/%r\]],
+            [[%E%f:%l:%c: %m]],
+            [[%C%m]],
+            [[%Z%\d\+ problems]],
+            [[%Z%\s%#]],
+          },
+          rootMarkers = { ".textlintrc", ".textlintrc.json" },
+        },
       },
     },
   }
