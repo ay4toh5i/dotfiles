8a716c3c9e33e61045fd1debd3ba09399a8b4e0c
Ayato <ay4toh5i@gmail.com>
2025-10-30 22:52:40 +0900

    Remove specifying command in golangci_lint_ls.lua

--- .config/nvim/after/lsp/golangci_lint_ls.lua
+++ .config/nvim/after/lsp/golangci_lint_ls.lua
@@ -2,7 +2,4 @@
 return {
   cmd = { "golangci-lint-langserver" },
   root_markers = { "go.mod", ".git" },
-  init_options = {
-    -- command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false", "--issues-exit-code=1" },
-  },
 }
