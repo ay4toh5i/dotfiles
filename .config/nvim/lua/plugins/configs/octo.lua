33c38174176fa1f86e746df375ee2d3c228a3a79
Ayato Higuchi <higuchi@sansan.com>
2025-11-04 22:04:27 +0900

    Add octo.nvim

---
+++ .config/nvim/lua/plugins/configs/octo.lua
@@ -0,0 +1,11 @@
+return {
+  'pwntester/octo.nvim',
+  config = function()
+    require("octo").setup()
+  end,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+}
