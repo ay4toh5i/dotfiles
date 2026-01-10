035d66d871ed2679e2e309f8e7875c15aae9cec7
Ayato <ay4toh5i@gmail.com>
2026-01-10 22:19:07 +0900

    Add fast-syntax-highlighting plugin instead of zsh-syntax-highlighting

--- home/.zshrc.d/10-plugins.zsh
+++ home/.zshrc.d/10-plugins.zsh
@@ -1,7 +1,6 @@
 source ~/dotfiles/home/antigen.zsh
 antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zdharma-continuum/fast-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
 antigen bundle olets/zsh-abbr@main
 antigen apply
-
