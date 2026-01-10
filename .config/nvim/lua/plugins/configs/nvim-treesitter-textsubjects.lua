f067971f83d4b32605d603d79f523558a2d5308f
Ayato <ay4toh5i@gmail.com>
2026-01-10 21:00:54 +0900

    Disable nvim-treesitter-textsubjects plugin temporarily

--- .config/nvim/lua/plugins/configs/nvim-treesitter-textsubjects.lua
+++ .config/nvim/lua/plugins/configs/nvim-treesitter-textsubjects.lua
@@ -1,5 +1,6 @@
 return {
   'rrethy/nvim-treesitter-textsubjects',
+  enabled = false, -- enable when this plugin supports nvim-treesitter main branch(https://github.com/RRethy/nvim-treesitter-textsubjects/issues/52).
   config = function()
     require('nvim-treesitter-textsubjects').configure({
       prev_selection = ',',
