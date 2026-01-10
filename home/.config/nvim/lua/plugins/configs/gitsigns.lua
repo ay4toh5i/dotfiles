4ee05a416ee61e33f6f51385ff400465b4bab8da
Ayato <ay4toh5i@gmail.com>
2026-01-10 22:11:52 +0900

    Add gitsigns keybindings to change base

--- home/.config/nvim/lua/plugins/configs/gitsigns.lua
+++ home/.config/nvim/lua/plugins/configs/gitsigns.lua
@@ -11,5 +11,7 @@ return {
     { '<leader>gh', '<cmd>Gitsigns preview_hunk<cr>', desc = 'Preview Hunk' },
     { '<leader>gr', '<cmd>Gitsigns reset_hunk<cr>',   desc = 'Reset Hunk' },
     { '<leader>gR', '<cmd>Gitsigns reset_buffer<cr>', desc = 'Reset Buffer' },
+    { '<leader>gm', '<cmd>Gitsigns change_base main<cr>', desc = 'Change Base to Main' },
+    { '<leader>gH', '<cmd>Gitsigns change_base HEAD<cr>', desc = 'Change Base to HEAD' },
   },
 }
