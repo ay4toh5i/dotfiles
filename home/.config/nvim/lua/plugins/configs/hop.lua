21f43fecf60f7fc3b60ae412c819a96d843aa95c
Ayato <ay4toh5i@gmail.com>
2026-01-11 12:08:29 +0900

    Improve hop.nvim keybindings

--- home/.config/nvim/lua/plugins/configs/hop.lua
+++ home/.config/nvim/lua/plugins/configs/hop.lua
@@ -1,13 +1,14 @@
 return {
-    'smoka7/hop.nvim',
-    version = "*",
-    config = function ()
-      require('hop').setup()
-      vim.keymap.set('n', '<leader><leader>w', '<cmd>HopWordAC<cr>')
-      vim.keymap.set('n', '<leader><leader>W', '<cmd>HopCamelCaseAC<cr>')
-      vim.keymap.set('n', '<leader><leader>b', '<cmd>HopWordBC<cr>')
-      vim.keymap.set('n', '<leader><leader>B', '<cmd>HopCamelCaseBC<cr>')
-      vim.keymap.set('n', '<leader><leader>j', '<cmd>HopLineStartAC<cr>')
-      vim.keymap.set('n', '<leader><leader>k', '<cmd>HopLineStartBC<cr>')
-    end,
+  'smoka7/hop.nvim',
+  version = "*",
+  config = function()
+    require('hop').setup()
+    vim.keymap.set('n', '<leader><leader>w', '<cmd>HopWordAC<cr>')
+    vim.keymap.set('n', '<leader><leader>W', '<cmd>HopCamelCaseAC<cr>')
+    vim.keymap.set('n', '<leader><leader>.', '<cmd>HopWord<cr>')
+    vim.keymap.set('n', '<leader><leader>b', '<cmd>HopWordBC<cr>')
+    vim.keymap.set('n', '<leader><leader>B', '<cmd>HopCamelCaseBC<cr>')
+    vim.keymap.set('n', '<leader><leader>j', '<cmd>HopLineStartAC<cr>')
+    vim.keymap.set('n', '<leader><leader>k', '<cmd>HopLineStartBC<cr>')
+  end,
 }
