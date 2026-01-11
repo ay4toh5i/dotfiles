b886253779d38f37a8e9b69ed6f5cd53f6d55871
Ayato <ay4toh5i@gmail.com>
2026-01-11 11:44:52 +0900

    Add git-conflict memo

--- home/.config/nvim/lua/plugins/configs/git-conflict.lua
+++ home/.config/nvim/lua/plugins/configs/git-conflict.lua
@@ -1,3 +1,20 @@
+-- Commands:
+-- GitConflictChooseOurs — Select the current changes.
+-- GitConflictChooseTheirs — Select the incoming changes.
+-- GitConflictChooseBoth — Select both changes.
+-- GitConflictChooseNone — Select none of the changes.
+-- GitConflictNextConflict — Move to the next conflict.
+-- GitConflictPrevConflict — Move to the previous conflict.
+-- GitConflictListQf — Get all conflict to quickfix
+
+-- Mappings:
+-- co — choose ours
+-- ct — choose theirs
+-- cb — choose both
+-- c0 — choose none
+-- ]x — move to previous conflict
+-- [x — move to next conflict
+
 return {
   'akinsho/git-conflict.nvim',
   version = "*",
