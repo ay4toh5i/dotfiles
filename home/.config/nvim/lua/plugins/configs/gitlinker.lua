3eab47cca129e3d176567d68eb8caa377eec790d
Ayato <ay4toh5i@gmail.com>
2026-01-11 11:44:33 +0900

    Improve gitlinker key mappings

--- home/.config/nvim/lua/plugins/configs/gitlinker.lua
+++ home/.config/nvim/lua/plugins/configs/gitlinker.lua
@@ -4,7 +4,28 @@ return {
     require('gitlinker').setup()
   end,
   keys = {
-    { '<leader>go', '<cmd>lua require("gitlinker").get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>', desc = 'Get Git Link' },
+    {
+      '<leader>gy',
+      '<cmd>lua require("gitlinker").get_buf_range_url("n")<cr>',
+      desc = 'Copy Git Link',
+    },
+    {
+      '<leader>gy',
+      '<cmd>lua require("gitlinker").get_buf_range_url("n")<cr>',
+      mode = 'v',
+      desc = 'Copy Git Link',
+    },
+    {
+      '<leader>go',
+      '<cmd>lua require("gitlinker").get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+      desc = 'Open Git Link',
+    },
+    {
+      '<leader>go',
+      '<cmd>lua require("gitlinker").get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+      mode = 'v',
+      desc = 'Open Git Link',
+    },
   },
   dependencies = {
     'nvim-lua/plenary.nvim',
