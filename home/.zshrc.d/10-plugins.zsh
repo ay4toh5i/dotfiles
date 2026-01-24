b7da2a23857c5e4a320f113974cee0c300e4b515
Ayato <ay4toh5i@gmail.com>
2026-01-25 01:46:23 +0900

    Add fzf-tab-completion zsh plugin and load it in .zshrc

--- home/.zshrc.d/10-plugins.zsh
+++ home/.zshrc.d/10-plugins.zsh
@@ -9,11 +9,20 @@ ZSH_PLUGINS=$HOME/.confg/.zshrc.d/plugins
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
+# fzf-tab-completion plugin
+if [[ ! -d "$ZSH_PLUGINS/fzf-tab-completion" ]]; then
+  git clone --depth 1 https://github.com/lincheney/fzf-tab-completion "$ZSH_PLUGINS/fzf-tab-completion"
+fi
+
+if [[ -f "$ZSH_PLUGINS/fzf-tab-completion/zsh/fzf-zsh-completion.sh" ]]; then
+  source $ZSH_PLUGINS/fzf-tab-completion/zsh/fzf-zsh-completion.sh
+fi
+
+# zeno plugin
 if [[ ! -d "$ZSH_PLUGINS/zeno" ]]; then
   git clone --depth 1 https://github.com/yuki-yano/zeno.zsh.git "$ZSH_PLUGINS/zeno"
 fi
 
-# Load zeno.zsh plugin If zno.zsh exists
 if [[ -f "$ZSH_PLUGINS/zeno/zeno.zsh" ]]; then
   source $ZSH_PLUGINS/zeno/zeno.zsh
 fi
