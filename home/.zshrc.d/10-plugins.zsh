0859249cc34b24e27268678f62615e5c7f87f6b7
Ayato <ay4toh5i@gmail.com>
2026-01-25 00:17:36 +0900

    Add zeno plugin and configuration and disable abbr plugin

--- home/.zshrc.d/10-plugins.zsh
+++ home/.zshrc.d/10-plugins.zsh
@@ -2,10 +2,18 @@ source ~/dotfiles/home/antigen.zsh
 antigen bundle zsh-users/zsh-completions
 antigen bundle zdharma-continuum/fast-syntax-highlighting
 antigen bundle zsh-users/zsh-autosuggestions
-antigen bundle olets/zsh-abbr@main
 antigen apply
 
 # Instaill zsh plugins which are not managed by antigen
 ZSH_PLUGINS=$HOME/.confg/.zshrc.d/plugins
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+if [[ ! -d "$ZSH_PLUGINS/zeno" ]]; then
+  git clone --depth 1 https://github.com/yuki-yano/zeno.zsh.git "$ZSH_PLUGINS/zeno"
+fi
+
+# Load zeno.zsh plugin If zno.zsh exists
+if [[ -f "$ZSH_PLUGINS/zeno/zeno.zsh" ]]; then
+  source $ZSH_PLUGINS/zeno/zeno.zsh
+fi
--- home/.zshrc.d/30-abbr.zsh
+++ home/.zshrc.d/30-abbr.zsh
@@ -1,11 +1,13 @@
 # register auto-expanding abbreviations
-ABBR_SET_EXPANSION_CURSOR=1
-abbr -S gsp="git switch -" > /dev/null
-abbr -S gcb="git checkout -b %" > /dev/null
-abbr -S gcbm="git checkout -b % main" > /dev/null
-abbr -S gw="BRANCH=% && git worktree add ./worktrees/\$BRANCH -b \$BRANCH main && cd worktrees/\$BRANCH" > /dev/null
-abbr -S gwm="cd \$(git worktree list | awk '{print \$1}' | fzf --reverse)" > /dev/null
-abbr -S gwt="cd \$(git worktree list | awk '{print \$1}' | awk 'NR==1||length<m{m=length;s=\$0}END{print s}')" > /dev/null
-abbr -S gwr="git worktree remove" >  /dev/null
+if builtin command -v abbr &>/dev/null; then
+  ABBR_SET_EXPANSION_CURSOR=1
+  abbr -S gsp="git switch -" > /dev/null
+  abbr -S gcb="git checkout -b %" > /dev/null
+  abbr -S gcbm="git checkout -b % main" > /dev/null
+  abbr -S gw="BRANCH=% && git worktree add ./worktrees/\$BRANCH -b \$BRANCH main && cd worktrees/\$BRANCH" > /dev/null
+  abbr -S gwm="cd \$(git worktree list | awk '{print \$1}' | fzf --reverse)" > /dev/null
+  abbr -S gwt="cd \$(git worktree list | awk '{print \$1}' | awk 'NR==1||length<m{m=length;s=\$0}END{print s}')" > /dev/null
+  abbr -S gwr="git worktree remove" >  /dev/null
 
-abbr -S gal="gcloud auth login --update-adc" > /dev/null
+  abbr -S gal="gcloud auth login --update-adc" > /dev/null
+fi
