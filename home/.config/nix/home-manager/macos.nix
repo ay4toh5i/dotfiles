6bdb882cf73764f5c89883a7e392693126c297ae
Ayato <ay4toh5i@gmail.com>
2026-02-07 21:55:55 +0900

    Format

--- home/.config/nix/home-manager/macos.nix
+++ home/.config/nix/home-manager/macos.nix
@@ -4,9 +4,11 @@
   config,
   pkgs,
   ...
-}: let
+}:
+let
   username = "ayato";
-in {
+in
+{
   nixpkgs = {
     config = {
       allowUnfree = true;
@@ -24,42 +26,42 @@ in {
     stateVersion = "26.05";
 
     packages = with pkgs; [
-    bat
-    bottom
-    delta
-    eza
-    exiftool
-    fd
-    fzf
-    ffmpeg
-    gh
-    gcc
-    gawk
-    gnused
-    gnupg
-    git
-    gibo
-    ghq
-    go-task
-    jq
-    k6
-    mise
-    nkf
-    nixfmt
-    neovim # nightly
-    ripgrep
-    starship
-    tig
-    tree
-    tmux
-    tlrc # tldr
-    vim
-    yazi
-    yq
-    yubikey-manager
-    wezterm
-    zoxide
-  ];
+      bat
+      bottom
+      delta
+      eza
+      exiftool
+      fd
+      fzf
+      ffmpeg
+      gh
+      gcc
+      gawk
+      gnused
+      gnupg
+      git
+      gibo
+      ghq
+      go-task
+      jq
+      k6
+      mise
+      nkf
+      nixfmt
+      neovim # nightly
+      ripgrep
+      starship
+      tig
+      tree
+      tmux
+      tlrc # tldr
+      vim
+      yazi
+      yq
+      yubikey-manager
+      wezterm
+      zoxide
+    ];
   };
 
   programs.home-manager.enable = true;
