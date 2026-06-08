f470238699081c7602b7f4b150d35497540956f0
Ayato <ay4toh5i@gmail.com>
2026-06-08 18:30:02 +0900

    Fix efm textlint command to use project local package insteadof global one.

--- home/.config/nvim/after/lsp/efm.lua
+++ home/.config/nvim/after/lsp/efm.lua
@@ -17,7 +17,7 @@ return {
           prefix = "textlint",
           lintCommand =
           -- "deno run -A npm:textlint@latest --no-color --format unix --stdin --stdin-filename ${INPUT}",
-          "npx textlint@latest --no-color --format unix --stdin --stdin-filename ${INPUT}",
+          "npx textlint --no-color --format unix --stdin --stdin-filename ${INPUT}",
           lintStdin = true,
           lintAfterOpen = true,
           lintFormats = {
